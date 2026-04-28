04ea003167ba1f2a9f48f4b00c42bd7672f3bec2
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-04-28 11:45:12 +0600

    News right side designed

--- src/app/(main)/details/[id]/page.jsx
+++ src/app/(main)/details/[id]/page.jsx
@@ -1,10 +1,27 @@
+import MyContainer from "@/components/Container/MyContainer";
+import LoginWith from "@/components/Home/LoginWith";
 import React from "react";
+const getNews = async (newsID) => {
+  const res = await fetch(
+    `https://openapi.programming-hero.com/api/news/${newsID}`,
+  );
+  const data = await res.json();
+  return data.data[0];
+};
 
-const DetailsPage = () => {
+const DetailsPage = async ({ params }) => {
+  const { id } = await params;
+  const news = await getNews(id);
+  console.log(news);
   return (
-    <div>
-      <p>Details page</p>
-    </div>
+    <MyContainer className="mt-7.5">
+      <div className="grid grid-cols-12 gap-6">
+        <div className="col-span-9">Details</div>
+        <div className="col-span-3">
+          <LoginWith />
+        </div>
+      </div>
+    </MyContainer>
   );
 };
 
