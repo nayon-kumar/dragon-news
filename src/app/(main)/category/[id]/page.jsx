b5ec1a768678662471d474a2c411e50242e598ea
Sree Nayon Kumar Pal <nayon.cse55@gmail.com>
2026-05-05 19:43:56 +0600

    Login page google and github signin added

--- src/app/(main)/category/[id]/page.jsx
+++ src/app/(main)/category/[id]/page.jsx
@@ -6,7 +6,6 @@ import React from "react";
 
 const Category = async ({ params }) => {
   const { id } = await params;
-  console.log(id);
   return (
     <div>
       <MyContainer className="grid grid-cols-1 mt-20 gap-6 md:grid-cols-12">
